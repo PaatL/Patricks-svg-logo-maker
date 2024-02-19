3d4b4c7feb0e64500f5e0177523c9bd6ddee3bf6
Patrick <lipatrick093@gmail.com>
2024-02-19 15:18:08 -0500

    working version

--- index.js
+++ index.js
@@ -1,38 +1,62 @@
 //require inquirer, path
 const inquirer = require("inquirer");
-const { join } = require("path");
 const { writeFile } = require("fs/promises");
-const shapes = require("./lib/shapes");
+const { Circle, Triangle, Square } = require("./lib/shapes");
 
-inquirer.prompt([
+inquirer
+  .prompt([
     //ask for input of 3 letters of text
-  {
-    type: "input",
-    name: "text",
-    message: "Please enter text (maximum 3 characters)",
-  },
-  //ask for text color
-  {
-    type: "input",
-    name: "textColor",
-    message: "Please enter your color choice for the text"
-  },
-  //ask for shape
-  {
-    type: "list",
-    choices: ["Circle","Triangle","Square"],
-    name: "shape",
-    message: "Please select which shape to render"
-  },
-  //ask for shape color
-  {
-    type: "input",
-    name: "shapeColor",
-    message: "Please enter your color choice for the shape"
-  }
-]).then((answers)=> {
+    {
+      type: "input",
+      name: "text",
+      message: "Please enter text (maximum 3 characters)",
+    },
+    //ask for text color
+    {
+      type: "input",
+      name: "textColor",
+      message: "Please enter your color choice for the text",
+    },
+    //ask for shape
+    {
+      type: "list",
+      choices: ["Circle", "Triangle", "Square"],
+      name: "shape",
+      message: "Please select which shape to render",
+    },
+    //ask for shape color
+    {
+      type: "input",
+      name: "shapeColor",
+      message: "Please enter your color choice for the shape",
+    },
+  ])
+  .then((answers) => {
     //collect responses
     console.log(answers);
+    let renderShape;
+    if (answers.shape === "Circle") {
+      renderShape = new Circle(
+        answers.text,
+        answers.textColor,
+        answers.shapeColor
+      );
+    } else if (answers.shape === "Triangle") {
+      renderShape = new Triangle(
+        answers.text,
+        answers.textColor,
+        answers.shapeColor
+      );
+    } else {
+      renderShape = new Square(
+        answers.text,
+        answers.textColor,
+        answers.shapeColor
+      );
+    }
+    writeFile("logo.svg", renderShape.render(), (err) =>
+      err ? console.log(err) : console.log("created svg")
+    );
     //render shape
     //make svg file
-})
+  });
